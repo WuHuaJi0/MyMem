28ee05f7b341b522c728eff55856bbf81517c834
wuhuaji0 <wuhuaji0@gmail.com>
2020-05-26 22:00:39 +0800

    Beautify ui.

--- src/components/Browser.vue
+++ src/components/Browser.vue
@@ -2,28 +2,38 @@
     <div>
         <el-container>
             <el-aside width="200px">
-                <p><i class="el-icon-s-promotion"></i>Keys <i class="el-icon-circle-plus" @click="toggleShowAdd"></i>
-                </p>
+                <div class="keysTitle">
+                    <div>
+                        <i class="el-icon-s-promotion"></i>Keys
+                    </div>
+                    <el-button icon="el-icon-plus" size="mini" @click="toggleShowAdd"></el-button>
+                </div>
                 <el-row type="flex" v-show="showAddNewKey">
                     <el-input v-model="newKey" placeholder="Enter key." size="small"></el-input>
                     <el-button size="small" @click="addNewKey">Confirm</el-button>
                 </el-row>
-                <p v-for="key in keys" v-text="key" @click="getValue(key)"></p>
+                <div class="keyItemContainer">
+                    <p class="keyItem" v-for="(key,index) in keys" @click="getValue(key,index)" :class="{ current : index == currentEditKey } ">
+                        <i class="el-icon-info"></i> {{ key }}
+                    </p>
+                </div>
             </el-aside>
 
             <el-main>
-                <el-input
-                        type="textarea"
-                        autosize
-                        placeholder="Please fill your content."
-                        v-model="currentValue">
-                </el-input>
-
-                <el-row>
-                    <el-button size="small" @click="saveChange" :disabled="originValue == currentValue">Save Change
-                    </el-button>
-                </el-row>
-
+                <div class="editArea" v-show="showEditArea">
+                    <el-input
+                            type="textarea"
+                            autosize
+                            placeholder="Please fill your content."
+                            v-model="currentValue">
+                    </el-input>
+                    <el-row>
+                        <el-button size="small" @click="saveChange" :disabled="originValue == currentValue">Save Change
+                        </el-button>
+                    </el-row>
+                </div>
+                <div class="plainArea" v-show="!showEditArea">
+                    <i class="el-icon-message-solid"></i>Select key in left panel.</div>
             </el-main>
         </el-container>
     </div>
@@ -40,22 +50,24 @@
         data() {
             return {
                 keys: [],
+                showEditArea: false,
                 originValue: "",
                 currentValue: "",
                 currentKey: "",
                 newKey: "",
                 showAddNewKey: false,
+                currentEditKey: null,
             }
         },
         async mounted() {
             try {
-                this.keys = await mainProcess.keys();
+                this.keys =  await mainProcess.keys();
             } catch (e) {
                 this.$message.error("Connect error");
                 let that = this;
                 setTimeout(function () {
                     that.$router.push("/");
-                },1500)
+                }, 1500)
             }
         },
         methods: {
@@ -73,9 +85,13 @@
             },
             toggleShowAdd() {
                 this.showAddNewKey = !this.showAddNewKey;
+                this.currentEditKey = null;
+                this.showEditArea = false;
             },
-            async getValue(key) {
+            async getValue(key,index) {
+                this.showEditArea = true;
                 this.currentKey = key;
+                this.currentEditKey = index;
                 try {
                     this.currentValue = this.originValue = await mainProcess.get(key)
                 } catch (err) {
@@ -100,24 +116,52 @@
 </script>
 
 <style scoped>
+    .keysTitle{
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+        color: #666666;
+        padding: 10px;
+    }
+    .keyItemContainer{
+        margin-top: 10px;
+    }
+    .keyItem{
+        color: #666;
+        padding: 5px 10px;
+        cursor: pointer;
+    }
+    .keyItem.current{
+        background: #999;
+        color: #fff;
+    }
+    .plainArea{
+        position: absolute;
+        margin: auto;
+        left: 0;
+        right: 0;
+        top: 0;
+        bottom: 0;
+        color: #999;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        font-size: 25px;
+    }
+    .plainArea i{
+        margin-right: 10px;
+    }
     .el-container {
         height: 100vh;
     }
 
+    .el-main{
+        position: relative;
+    }
     .el-aside {
         background: #eee;
-        padding: 10px;
     }
-
-    .connect-form {
-        width: 500px;
-        margin: 200px auto 0;
-        padding: 20px;
-        box-sizing: border-box;
-        background: #eeeeee;
-    }
-
     .el-row {
-        margin-top: 15px;
+        /*margin-top: 15px;*/
     }
 </style>
